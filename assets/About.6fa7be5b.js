f972253ca314d9d6a161c7b3ef8d718777f52297
daiki.s <daiki.s@itnav.co.jp>
2022-10-15 08:44:27 +0900

    Updates

---
+++ assets/About.6fa7be5b.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index.78a11054.js";const t={};function n(r,s){return c(),o("div",null,"About\u30DA\u30FC\u30B8\u3067\u3059")}const a=e(t,[["render",n]]);export{a as default};
---
+++ assets/Home.166c86b4.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index.78a11054.js";const n={};function r(t,s){return c(),o("div",null,"\u30DB\u30FC\u30E0\u3067\u3059")}const a=e(n,[["render",r]]);export{a as default};
